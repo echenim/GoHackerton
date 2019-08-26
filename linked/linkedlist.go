767d500e7258750506481c7c4309ac9f7a8f087c
Myron William Echenim <myron.echenim@live.com>
2019-08-26 00:12:41 -0700

    implementing append to single linkedlist and a receiver function for printing out the list

--- linked/linkedlist.go
+++ linked/linkedlist.go
@@ -1,27 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type node struct {
+//Node structure for linkedlist
+type Node struct {
 	data string
-	next *node
+	next *Node
 }
 
-func add(_data string) node {
-	return node{data: _data}
+func add(node *Node, _data string) *Node {
+
+	return &Node{data: _data, next: node}
 }
 
-func (n node) print() {
-
+func (n Node) print() {
+	fmt.Printf("%v ", n.data)
 	current := n.next
-	if current == nil {
-		fmt.Printf("%v --> null", n.data)
-	}
-	if current != nil {
-		for current.next != nil {
-			fmt.Printf("%v -->", n.data)
-			current = n.next
+	if current.next == nil {
+		//	fmt.Printf("%v --> null", n.data)
+	} else {
+		for current != nil {
+			fmt.Printf(" --> %v", current.data)
+			current = current.next
+
 		}
 	}
-
+	fmt.Print(" --> null")
 }
--- linked/main.go
+++ linked/main.go
@@ -1,8 +1,12 @@
 package main
 
 func main() {
+	var node *Node
 
-	_node := add("1")
-	k := _node
-	k.print()
+	node = add(node, "0")
+	node = add(node, "1")
+	node = add(node, "2")
+	node = add(node, "3")
+
+	node.print()
 }
