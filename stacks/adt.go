534c45e78a4d91210222ab3a09ba3d61c0dce86a
Myron William Echenim <myron.echenim@live.com>
2019-10-08 13:39:39 -0700

    stack push fucntion implemtation

--- stacks/adt.go
+++ stacks/adt.go
@@ -1,11 +1,17 @@
 package main
 
-import (
-	"unsafe"
-)
-
 type stack struct {
 	size int
 	top  int
-	arr  unsafe.Pointer
+	arr  []int
+}
+
+func (s stack) Push(a []int) stack {
+	var i int
+	s.top++
+	for i = 0; i < s.size; i++ {
+		s.arr[i] = a[i]
+		s.top++
+	}
+	return s
 }
--- stacks/main.go
+++ stacks/main.go
@@ -1,8 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
 	var st stack
 	st.size = 10
-	//st.array = [5]int
+	st.top = -1
+
+	st.arr = make([]int, st.size)
+	gen := datagen(st.size)
+	fmt.Printf("initial top :: %v\n", st.top)
+	k := st.Push(gen)
+	fmt.Printf("stack size :: %v\n", k.size)
+	fmt.Printf("current top :: %v\n", k.top)
+	fmt.Printf("stack from F->L :: %v", k.arr)
+
 }
 
+func datagen(size int) []int {
+	var i int
+	arr := make([]int, size, size)
+	for i = 0; i < size; i++ {
+		arr[i] = rand.Intn(1000)
+	}
+	fmt.Printf("generated data :: %v\n", arr)
+	return arr
+}
