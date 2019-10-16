28321ac20c15d8addfd9cd9cfe6f2c5d52d45d6a
Myron William Echenim <myron.echenim@live.com>
2019-10-16 13:21:11 -0700

    mmodify stack push function

--- stacks/main.go
+++ stacks/main.go
@@ -7,19 +7,33 @@ import (
 
 func main() {
 	var st stack
+
 	st.size = 10
 	st.top = -1
 
 	st.arr = make([]int, st.size)
 	gen := datagen(st.size)
 	fmt.Printf("initial top :: %v\n", st.top)
-	k := st.Push(gen)
-	fmt.Printf("stack size :: %v\n", k.size)
-	fmt.Printf("current top :: %v\n", k.top)
-	fmt.Printf("Is stack empty: %v\n", k.IsEmpty())
-	fmt.Printf("Is stack full: %v\n", k.IsFull())
-	fmt.Printf("stack from F->L :: %v\n", k.arr)
-	k.Pop()
+
+	fmt.Println("------ stack growth and information ---------")
+	for i := range gen {
+		value := gen[i]
+
+		k := st.Push(value)
+		// fmt.Printf("stack size :: %v\n", k.size)
+		// fmt.Printf("current top :: %v\n", k.top)
+		// fmt.Printf("Is stack empty: %v\n", k.IsEmpty())
+		// fmt.Printf("Is stack full: %v\n", k.IsFull())
+		fmt.Printf("stack from F->L :: %v\n", k.arr)
+
+	}
+	// //k := st.Push(gen)
+	// fmt.Printf("stack size :: %v\n", k.size)
+	// fmt.Printf("current top :: %v\n", k.top)
+	// fmt.Printf("Is stack empty: %v\n", k.IsEmpty())
+	// fmt.Printf("Is stack full: %v\n", k.IsFull())
+	// fmt.Printf("stack from F->L :: %v\n", k.arr)
+	// k.Pop()
 
 }
 
@@ -29,9 +43,6 @@ func datagen(size int) []int {
 	for i = 0; i < size; i++ {
 		arr[i] = rand.Intn(1000)
 	}
-	fmt.Printf("generated data :: %v\n", arr)
-	return arr
-}
 	fmt.Printf("generated data :: %v\n", arr)
 	return arr
 }
