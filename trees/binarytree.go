954c5053a97f24d62530c18a240977f960df7b25
Myron William Echenim <myron.echenim@live.com>
2019-11-04 01:05:33 -0800

    disa tree data

--- trees/binarytree.go
+++ trees/binarytree.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 //Btree defination for single linked list
 type Btree struct {
 	root *Bnode
@@ -33,4 +37,16 @@ func (b *Bnode) insert(n *Bnode) {
 			b.rightChild.insert(n)
 		}
 	}
+}
+
+//Print fucntion for displaying the tree nodes and branch
+func (tree *Btree) Print(b *Bnode, ns int, ch string) {
+
+	if b == nil {
+		return
+	}
+	fmt.Printf("%v:%v\n", ch, b.data)
+	tree.Print(b.leftChild, ns+2, "L")
+	tree.Print(b.rightChild, ns+2, "R")
+
 }
--- trees/main.go
+++ trees/main.go
@@ -2,12 +2,13 @@ package main
 
 func main() {
 
-	tree := &Btree{}
+	tree := Btree{}
 	tree.Insert(&Bnode{data: "echenim", leftChild: nil, rightChild: nil})
 	tree.Insert(&Bnode{data: "william", leftChild: nil, rightChild: nil})
 	tree.Insert(&Bnode{data: "chukwuma", leftChild: nil, rightChild: nil})
 	tree.Insert(&Bnode{data: "christian", leftChild: nil, rightChild: nil})
 	tree.Insert(&Bnode{data: "physicist", leftChild: nil, rightChild: nil})
 	tree.Insert(&Bnode{data: "myron", leftChild: nil, rightChild: nil})
+	tree.Print(tree.root, 0, "M")
 
 }
