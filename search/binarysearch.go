31b6fbf0d7bb1c73751343d51542ae9549120592
Myron William Echenim <myron.echenim@live.com>
2019-11-07 00:05:04 -0800

    implement count and heigh size

--- search/binarysearch.go
+++ search/binarysearch.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //Btree defination
 type Btree struct {
 	root *Bnode
@@ -35,4 +37,46 @@ func (b *Bnode) insert(node *Bnode) {
 			b.rightChild.insert(node)
 		}
 	}
+}
+
+//Print fucntion for displaying the tree nodes and branch
+func (tree *Btree) Print(b *Bnode, ns int, ch string) {
+
+	if b == nil {
+		return
+	}
+	fmt.Printf("%v:%v\n", ch, b.data)
+	tree.Print(b.leftChild, ns+2, "L")
+	tree.Print(b.rightChild, ns+2, "R")
+
+}
+
+//Count fucntion for binary tree
+func Count(n *Bnode) int {
+
+	if n == nil {
+		return 0
+	}
+
+	return Count(n.rightChild) + Count(n.leftChild) + 1
+}
+
+//Height fucntion for binary tree
+func Height(n *Bnode) int {
+
+	x := 0
+	y := 0
+
+	if n == nil {
+		return 0
+	}
+
+	x = Height(n.leftChild)
+	y = Height(n.rightChild)
+	if x > y {
+		return x + 1
+	} else {
+		return y + 1
+	}
+
 }
--- search/main.go
+++ search/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	tree := Btree{}
@@ -15,5 +17,10 @@ func main() {
 	tree.Insert(&Bnode{data: 45, leftChild: nil, rightChild: nil})
 
 	tree.Insert(&Bnode{data: 60, leftChild: nil, rightChild: nil})
+
+	tree.Print(tree.root, 0, "M")
+	fmt.Printf("Number of node or branch in this binary tree :  %v\n", Count(tree.root))
+	fmt.Printf("Height of this binary tree :  %v\n", Height(tree.root))
+	//Count(tree.root)
 
 }
