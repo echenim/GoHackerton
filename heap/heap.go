5648ce87e41632d87b2e9a2110cf4abe7c1088c3
Myron William Echenim <myron.echenim@live.com>
2020-01-25 23:43:45 -0800

    implemetation delete function for heap

---
+++ heap/heap.go
@@ -0,0 +1,43 @@
+package main
+
+//Insert function for heap
+func Insert(arr []int, size int) {
+
+	i := size
+	temp := arr[i]
+
+	for i > 0 && temp > arr[i/2] {
+		arr[i] = arr[i/2]
+		i = i / 2
+	}
+	arr[i] = temp
+
+}
+
+//Delete function for heap
+//Delete function by deleting the root value and transfer it to the end of the array
+func Delete(arr []int, size int) int {
+
+	//x := arr[size]
+	val := arr[0]
+	arr[0] = arr[size]
+	//assign the delete val to the end of the array
+	arr[size] = val
+	i := 0
+	j := i * 2
+	for j < size-1 {
+		if arr[j+1] > arr[j] {
+			j = j + 1
+		}
+
+		if arr[i] < arr[j] {
+			temp := arr[i]
+			arr[i] = arr[j]
+			arr[j] = temp
+			i = j
+			j = 2 * j
+		}
+	}
+
+	return val
+}
--- heap/main.go
+++ heap/main.go
@@ -4,11 +4,18 @@ import "fmt"
 
 func main() {
 
-	H := []int{0, 2, 5, 8, 9, 4, 10, 7}
+	H := []int{2, 10, 20, 30, 25, 5, 40, 35}
+	fmt.Println("......Original data .........")
 	fmt.Println(H)
-	for i := 2; i <= len(H)-1; i++ {
+	for i := 0; i <= len(H)-1; i++ {
 		Insert(H, i)
-		fmt.Println(H)
 	}
+	fmt.Println("......Heap Array formed .........")
+	fmt.Println(H)
+	fmt.Println("......deleted data .........")
+	k := Delete(H, len(H)-1)
+	fmt.Println(k)
+	fmt.Println("......After deleted Heap Array formed .........")
+	fmt.Println(H)
 
 }
